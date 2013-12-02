a71d59df4675360f8403432c9dca78d05e7ced23
dan tsai <tsai.dan@gmail.com>
2013-12-02 14:26:59 -0800

    db updates

--- db.php
+++ db.php
@@ -1,100 +1,67 @@
 <?php
- // connects to my ischool mysql database. username: jenton, password: qwerty, database: jenton
- $con = mysqli_connect("localhost", "dantos_ischool", "o2lb_awsIS", "dantos_dantsai");
+// connects to my ischool mysql database. username: jenton, password: qwerty, database: jenton
+$con = mysqli_connect("localhost", "dantos_ischool", "o2lb_awsIS", "dantos_dantsai");
  
- // Failure check
+// Failure check
 if (mysqli_connect_errno($con))
 {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-//grabbing data from the POST
-$bizID = $_POST["bizID"];
-//store the reviewIDs from the page. Looks like a string "1,2,3,4,5", so need to split it into a PHP array.
-$idArray = $_POST["reviewIDs"];
-$idArray = explode(",", $idArray);
- 
-$result = mysqli_query($con,"SELECT * FROM yelp");
-while($row = mysqli_fetch_array($result)) {
-  echo $row[0] . ' ' . $row[1] . ' ' . $row[2] . ' ' . $row[3] . ' ' . $row[4] . '<br>';
-} 
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+	if($_GET['action'] == 'getall') {
+		$result = mysqli_query($con,"SELECT * FROM facebook");
+		echo "scoreid guesserid targetid score<br>";
+		while($row = mysqli_fetch_array($result)) {
+			echo $row[0] . ' ' . $row[1] . ' ' . $row[2] . ' ' . $row[3] . ' ' . $row[4] . '<br>';
+		}
+	}
 
- //this code runs only when action == food from the ajax data call
-if ($_POST["action"] == "food") {
-   //the "reviewID" comes from data: in the ajax call
-   $reviewID = $_POST["reviewID"];
-  
-  //insert the reviewID and corresponding tag count into the yelp table
-   //if the key is a duplicate, then it increments the appropriate reviewID tag by 1
-  $query=mysqli_query($con,"INSERT INTO yelp VALUES ('$bizID', '$reviewID', 1, 0, 0, 0) 
-    ON DUPLICATE KEY UPDATE food=food + 1");
-  //updates the page with the new tag counts
-  updateTagCounts($idArray, $con);
+	if($_GET['action'] == 'topscores') {
+		$result = mysqli_query($con,"SELECT * FROM facebook ORDER BY score DESC LIMIT 20");
+		$rows = array();
+		while($r = mysqli_fetch_assoc($result)) {
+			$rows[] = $r;
+		}
+		echo json_encode($rows);
+	}
+
+	if($_GET['action'] == 'myscores') {
+		$guesserid = $_GET['guesserid'];
+		$result = mysqli_query($con,"SELECT * FROM facebook WHERE guesserid = $guesserid ORDER BY score DESC LIMIT 20");
+		$rows = array();
+		while($r = mysqli_fetch_assoc($result)) {
+			$rows[] = $r;
+		}
+		echo json_encode($rows);
+	}
+
+	if($_GET['action'] == 'targetscores') {
+		$targetid = $_GET['targetid'];
+		$result = mysqli_query($con,"SELECT * FROM facebook WHERE targetid = $targetid ORDER BY score DESC LIMIT 20");
+		$rows = array();
+		while($r = mysqli_fetch_assoc($result)) {
+			$rows[] = $r;
+		}
+		echo json_encode($rows);
+	}
+
+	if($_GET['action'] == 'toptotals') {
+		$result = mysqli_query($con,"SELECT guesserid, SUM(score) as score FROM facebook GROUP BY guesserid order by SUM(score) desc limit 20");
+		$rows = array();
+		while($r = mysqli_fetch_assoc($result)) {
+			$rows[] = $r;
+		}
+		echo json_encode($rows);
+	}
 }
 
-//this code runs only when the service button is clicked. It increments service by one for the reviewID
-if ($_POST["action"] == "service") {
-  //the "reviewID" comes from data: in the ajax call
-  $reviewID = $_POST["reviewID"];
-
-  //insert the reviewID and corresponding tag count into the yelp table
-  //if the key is a duplicate, then it increments the appropriate reviewID tag by 1
-  $query=mysqli_query($con,"INSERT INTO yelp VALUES ('$bizID', '$reviewID', 0, 1, 0, 0) 
-    ON DUPLICATE KEY UPDATE service=service + 1");
-  //updates the page with the new tag counts
-  updateTagCounts($idArray, $con);
+if($_POST['action'] == 'newscore') {
+	$guesserid = $_POST['guesserid'];
+	$targetid = $_POST['targetid'];
+	$score = $_POST['score'];
+	$result = mysqli_query($con,"INSERT INTO facebook (guesserid, targetid, score) VALUES ($guesserid, $targetid, $score)");
+	echo 'done';
 }
 
-//this code runs only when the atmosphere button is clicked. It increments atmosphere by one for the reviewID
-if ($_POST["action"] == "atmosphere") {
-  //the "reviewID" comes from data: in the ajax call
-  $reviewID = $_POST["reviewID"];
-
-  //insert the reviewID and corresponding tag count into the yelp table
-  //if the key is a duplicate, then it increments the appropriate reviewID tag by 1
-  $query=mysqli_query($con,"INSERT INTO yelp VALUES ('$bizID', '$reviewID', 0, 0, 1, 0) 
-    ON DUPLICATE KEY UPDATE atmosphere=atmosphere + 1");
-  //updates the page with the new tag counts
-  updateTagCounts($idArray, $con);
-}
-
-//this code runs only when the price button is clicked. It increments price by one for the reviewID
-if ($_POST["action"] == "price") {
-  //the "reviewID" comes from data: in the ajax call
-  $reviewID = $_POST["reviewID"];
-
-  //insert the reviewID and corresponding tag count into the yelp table
-  //if the key is a duplicate, then it increments the appropriate reviewID tag by 1
-  $query=mysqli_query($con,"INSERT INTO yelp VALUES ('$bizID', '$reviewID', 0, 0, 0, 1) 
-    ON DUPLICATE KEY UPDATE price=price + 1");
-  //updates the page with the new tag counts
-  updateTagCounts($idArray, $con);
-}
-
-//this runs on page load
-if ($_POST["action"] == "pageload") {
-  //updates the page with the new food and service counts
-  updateTagCounts($idArray, $con);
-
-}
-
-function updateTagCounts($Array, $con){
- //the following code iterates through every row in yelp and traverses through every item in idArray
-//if the ids match, then it will output the food and service counts
-//this is possibly the most inefficient way to do this...
-$result = mysqli_query($con,"SELECT * FROM yelp");
-while($row = mysqli_fetch_array($result)) {
-  foreach ($Array as $id) {
-    if ($id == $row['reviewID']) {
-      //this will return data in the script.js that looks like &ID=1&food=12&service=1
-      //maybe we can parse that and do something with it?
-      echo "ID=" . $id . "&food=" . $row['food'] . "&service=" . $row['service'] . "&atmosphere="
-        . $row['atmosphere'] . "&price=" . $row['price'] . ";";
-      //echo "reviewID: " . $row['reviewID'] . " food: " . $row['food'] . " service: " . $row['service'];
-      //echo "<br>";
-      }
-    }
-  } 
-} 
- 
 ?>
--- dbtest.js
+++ dbtest.js
@@ -1,15 +1,93 @@
 $(document).ready(function(){
-	$.ajax({
-		type:"post",
-		url:"db.php",
-		data: {
-			action: 'post',
-			id: '12345',
-			score: '54321'
-		} ,
-		success:function(data){
-			console.log(data);
-			alert('done');
+	updateAllScores();
+
+	// insert row to db
+	$('#postbutton').click(function() {
+		var guesserid = $('#guesserid').val();
+		var targetid = $('#targetid').val();
+		var score = $('#score').val();
+
+		if(!guesserid || !targetid || !score) {
+			alert('no input');
 		}
+
+		$.post('db.php', {
+				action: 'newscore',
+				guesserid: guesserid,
+				targetid: targetid,
+				score: score
+			}, function(data) {
+				console.log(data);
+				updateAllScores();
+				alert('done');
+			});
+	});
+
+
+	// get top individual scores
+	$('#topscores').click(function() {
+		$.get('db.php', {action: 'topscores'}, function(data) {
+			var values = JSON.parse(data);
+			var resultshtml = '';
+			for(var i = 0 ; i < values.length ; ++i) {
+				var v = values[i];
+				resultshtml = resultshtml + v.scoreid + " " + v.guesserid + " " + v.targetid + ' ' + v.score + '<br>';
+			}
+			$('#results').html(resultshtml);
+		});
+	});
+
+
+	// get top scores for guesser
+	$('#myscoresbutton').click(function() {
+		var guesserid = $('#myscores').val();
+		if(!guesserid)
+			alert("no guesser");
+		$.get('db.php', {action: 'myscores', guesserid: guesserid}, function(data) {
+			var values = JSON.parse(data);
+			var resultshtml = '';
+			for(var i = 0 ; i < values.length ; ++i) {
+				var v = values[i];
+				resultshtml = resultshtml + v.scoreid + " " + v.guesserid + " " + v.targetid + ' ' + v.score + '<br>';
+			}
+			$('#results').html(resultshtml);
+		});
+	});
+
+	// get top scores for target
+	$('#targetscoresbutton').click(function() {
+		var targetid = $('#targetscores').val();
+		if(!targetid)
+			alert("no target");
+		$.get('db.php', {action: 'targetscores', targetid: targetid}, function(data) {
+			var values = JSON.parse(data);
+			var resultshtml = '';
+			for(var i = 0 ; i < values.length ; ++i) {
+				var v = values[i];
+				resultshtml = resultshtml + v.scoreid + " " + v.guesserid + " " + v.targetid + ' ' + v.score + '<br>';
+			}
+			$('#results').html(resultshtml);
+		});
+	});
+
+
+	// get top aggregate scores
+	$('#toptotals').click(function() {
+		$.get('db.php', {action: 'toptotals'}, function(data) {
+			var values = JSON.parse(data);
+			var resultshtml = '';
+			for(var i = 0 ; i < values.length ; ++i) {
+				var v = values[i];
+				resultshtml = resultshtml + v.guesserid + ' ' + v.score + '<br>';
+			}
+			$('#results').html(resultshtml);
+		});
+	});
+
+});
+
+function updateAllScores() {
+	$.get('db.php', {action: 'getall'}, function(data) {
+		$('#text').html(data);
 	});
 }
