433613bacb0df7b2017d045cb105aa0ed121eee9
Jenton <Jenton@Jentons-MacBook-Pro.local>
2013-12-09 11:42:12 -0800

    JENTRON

--- index.php
+++ index.php
@@ -125,7 +125,7 @@
   <div id="footer">
     <p>
       THIS WAS MADE BY US. WE DID THIS. 
-      <a href="http://www.jentonlee.com">JENTARD LEE</a>, 
+      <a href="http://www.jentonlee.com">JENTRON LEE</a>, 
       <a href="http://www.dantsai.com/">DAN THE MAN TSAI</a>, 
       <a href="http://www.jooddang.com/">FUNKWANG JOO</a>, 
       <a href="http://www.wordium.com">SANDRAWR HELSLEY</a>
